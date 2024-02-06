0ce4544247d15348f8f384fca9df9cc18b6ec363
Leo Narinians <leonarinians@gmail.com>
2024-02-05 21:20:52 -0800

    in branch

---
+++ new.java
@@ -0,0 +1,8 @@
+/*-------------------------------------------------------------
+Leo Narinians
+Assignment4.java
+SPECIFICATION: Asks user to input numbers and outputs if that number is a prime number or not. 
+Adds all numbers entered by user, while also keeping count, and outputs max, min, count, and average.
+FOR: CS 1400- Assignment #4
+TIME SPENT: 45 minutes
+//-----------------------------------------------------------*/
